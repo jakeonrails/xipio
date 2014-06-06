77457227ff4d29cef7c18657df4cc9d734454c39
Jake Moffatt <jakeonrails@gmail.com>
2014-06-05 18:32:49 -0700

    Get the bin "xipio" working

--- lib/xipio.rb
+++ lib/xipio.rb
@@ -2,20 +2,26 @@ require "xipio/version"
 require 'socket'
 
 module Xipio
-  def ip_address
-    Socket.ip_address_list.detect{|intf| intf.ipv4_private?}.ip_address
-  end
 
-  def app_name
-    ARGV[0] || File.basename(Dir.getwd)
-  end
+  class Opener
 
-  def xip_io
-    "http://#{app_name}.#{ip_address}.xip.io"
-  end
+    def open
+      `open #{xip_io}`
+    end
 
-  def run
-    `open #{xip_io}`
+    private
+
+    def ip_address
+      Socket.ip_address_list.detect{|intf| intf.ipv4_private?}.ip_address
+    end
+
+    def app_name
+      ARGV[0] || File.basename(Dir.getwd)
+    end
+
+    def xip_io
+      "http://#{app_name}.#{ip_address}.xip.io"
+    end
   end
 
 end
